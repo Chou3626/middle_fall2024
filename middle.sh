3829d26ee34bcee29773824248f6ff00d22d22e1
Sung Chou <schou@ou.edu>
2024-09-27 15:09:43 -0500

    added how to use this script

--- middle.sh
+++ middle.sh
@@ -1,5 +1,7 @@
 # script to return the middle text of a file
 # created Sep 27, 2024
 # creator Sung Chou
+# Usage: 
+# include in first position, file path and name via command line
 
 head -n 9 "$1" | tail -n 3
